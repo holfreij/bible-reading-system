82874d9def67fcdb290e4d567914f71344a41a91
Rolf Heij <rolf_heij@sil.org>
2024-11-18 01:12:36 +0100

    Small supabase start

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
 import Lists from "./Lists";
 import Header from "./Header";
+import { createClient } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_PROJECT_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
 
 function App() {
+  const [countries, setCountries] = useState<string[]>([]);
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  useEffect(() => console.log(countries), [countries]);
+
+  async function getCountries() {
+    const { data } = await supabase.from("countries").select();
+    if (!data) return;
+    setCountries(data);
+  }
+
+  console.log();
+
   return (
     <div className="hero">
       <div className="hero-content flex-col">
